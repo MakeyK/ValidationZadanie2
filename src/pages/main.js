370f4fee12e54fc78b7a541a4ab74567ae84a8e9
MakeyK <zevstvsrgk@gmail.com>
2024-10-25 12:06:49 +0300

    изменения есть

--- src/pages/main.js
+++ src/pages/main.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import {observer} from "mobx-react-lite";
 import {Card, Container, Form, Button, Col} from 'react-bootstrap';
 import {MAIN_ROUTE} from '../utils/consts'
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -11,114 +11,72 @@ const MainPage = observer(() => {
     const location = useLocation()
     const isLogin = location.pathname === MAIN_ROUTE
     const [formData, setFormData] = useState({
-      email: '',
       name: '',
+      email: '',
       phone: '',
-      ip: '',
       address: '',
       message: '',
     });
 
     const [errors, setErrors] = useState('');
-    const handleChange = (e) => {
-      const { name, value } = e.target;
-      setFormData({ ...formData, [name]: value });
-    };
 
+    const Change = (e) => {
+      
+    }
 
-    const validate = () => {
-      const newErrors = {};
-      if (!formData.name) {
-        newErrors.name = 'Имя обязательно';
-      }
-      if (!formData.email) {
-        newErrors.email = 'Email обязателен';
-      } else if (!formData.email) {
-        newErrors.email = 'Email должен быть корректным';
-      }
-      if (!formData.phone) {
-        newErrors.phone = 'Телефон обязателен';
-      } else if (!formData.phone) {
-        newErrors.phone = 'Телефон должен содержать от 10 до 15 цифр';
-      }
-      if (!formData.ip) {
-        newErrors.ip = 'IP адрес обязателен';
-      } else {
-        newErrors.ip = 'Некорректный IP адрес';
-      }
-      if (!formData.address) {
-        newErrors.address = 'Адрес обязателен';
-      }
-      if (!formData.message) {
-        newErrors.message = 'Сообщение обязательно';
-      }
-      return newErrors;
-    };
+    const Submit = () => {
+      
+    }
+  
+    // useEffect(() => {
+    //   if (errors.length > 0) {
+    //     alert(errors.join('\n'));
+    //   }
+    // }, [errors]);
 
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      const validationErrors = validate();
-      if (Object.keys(validationErrors).length > 0) {
-        setErrors(validationErrors);
-      } else {
-        console.log('Данные отправлены:', formData);
-        setErrors({});
-      }
-    };
 
     return (
         <Container
         style = {{backgroundColor:'#D0D0D0', justifyContent:'space-between', position:'relative', padding:'20px', borderRadius: '15px', width: '1240px', height: '1100px', marginTop:'6px', fontFamily:"Play", paddingTop: '10px'}}>
         <Card style={{borderRadius: 80, fontFamily:"Play"}} className="p-5 #FFFAF4">
-        <Form className="d-flex flex-column" onSubmit={handleSubmit}>
+        <Form className="d-flex flex-column">
         <Form.Control
                 style={{borderRadius: 70, height: 71, border: "1px solid", fontSize: "24px", marginBottom:'20px'}}
                 className="mt-3"
                 placeholder = "Введите имя..."
                 size="lg"
-                onChange={handleChange}/>
-        <Form.Control
-                style={{borderRadius: 70, height: 71, border: "1px solid", fontSize: "24px", marginBottom:'20px'}}
-                className="mt-3"
-                placeholder = "Введите двухзначное число..."
-                size="lg"
-                onChange={handleChange}/>
+                type="text"
+                value={formData.name}
+                onChange={Change}/>
           <Form.Control
                 style={{borderRadius: 70, height: 71, border: "1px solid", fontSize: "24px", marginBottom:'20px'}}
                 className="mt-3"
                 placeholder = "Введите номер телефона..."
                 size="lg"
-                onChange={handleChange}/>
+                type="number"
+                // value={formData.phone}
+                onChange={Change}/>
           <Form.Control
                 style={{borderRadius: 70, height: 71, border: "1px solid", fontSize: "24px", marginBottom:'20px'}}
                 className="mt-3"
                 placeholder = "Введите email адрес..."
                 size="lg"
-                type="email"
-                onChange={handleChange}/>
-          <Form.Control
-                style={{borderRadius: 70, height: 71, border: "1px solid", fontSize: "24px", marginBottom:'20px'}}
-                className="mt-3"
-                placeholder = "Введите IP Адрес..."
-                size="lg"
-                onChange={handleChange}/>
-          <Form.Control
-                style={{borderRadius: 70, height: 71, border: "1px solid", fontSize: "24px", marginBottom:'20px'}}
-                className="mt-3"
-                placeholder = "Введите адрес сайта..."
-                size="lg"
-                onChange={handleChange}/>
+                type="email" 
+                value={formData.email}
+                onChange={Change}/>
           <Form.Control
                 style={{borderRadius: 40, height: 71, border: "1px solid", fontSize: "24px", marginBottom:'20px', height: '150px'}}
                 className="mt-3"
                 placeholder = "Ваще сообщение"
                 size="lg"
-                onChange={handleChange}/>
+                type="text" 
+                // value={formData.message}
+                onChange={Change}/>
                 <p style={{marginTop:'30px', display:'flex', justifyContent:'center'}}>
                 <Button
                             type="submit"
                             size={"lg"}
-                            onChange={handleChange}
+                            onChange={Submit}
                             variant={"outline-success"}
                             style={{fontWeight:'bold', borderRadius:37, width:'180px', height:'70px'}}
                             >
@@ -129,6 +87,7 @@ const MainPage = observer(() => {
         
         </Container>
     );
-});
+}
+);
 
 export default MainPage;
