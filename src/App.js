127be53d7a4a79e1b133c8a5a82a98e62b3b56fa
MakeyK <zevstvsrgk@gmail.com>
2024-10-28 13:47:53 +0300

    Задание выполнено

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter } from "react-router-dom"; 
 import AppRouter from "./components/AppRouter";  
+import MainPage from "./pages/main";
 
 function App() { 
   return ( 
     <BrowserRouter> 
 
       <AppRouter/> 
-      
+      <MainPage/>
     </BrowserRouter> 
   ); 
 } 
--- src/pages/main.js
+++ src/pages/main.js
@@ -1,85 +1,61 @@
 import React, { useState, useEffect } from "react";
 import {observer} from "mobx-react-lite";
 import {Card, Container, Form, Button, Col} from 'react-bootstrap';
-import {MAIN_ROUTE} from '../utils/consts'
-import { useLocation } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const MainPage = observer(() => {
-    document.body.style.backgroundColor="#fcfcee"
-    const location = useLocation()
-    const isLogin = location.pathname === MAIN_ROUTE
+    document.body.style.backgroundColor="#313131"
     const [formData, setFormData] = useState({
       name: '',
-      email: '',
       phone: '',
-      address: '',
-      message: '',
+      email: ''
     });
-    const [errors, setErrors] = useState('');
-
+    const [nameError, setNameError] = useState();
+    const [phoneError, setPhoneError] = useState();
+    const [emailError, setEmailError] = useState();
     useEffect(() => {
-      const newErrors = {};
-      for (const [key, value] of Object.entries(formData)) {
-        if (!value) {
-          newErrors[key] = 'Поле не может быть пустым';
-        }
-      }
-      setErrors(newErrors);
-    }, [formData]);
-  
-    const Change = (e) => {
-      const { name, value } = e.target;
-      setFormData({
-        ...formData,
-        [name]: value
-      });
-    };
-  
-    const Submit = (e) => {
-      e.preventDefault();
-      if (Object.keys(errors).length === 0) {
-        console.log('Данные отправлены:', formData);
-      }
-    };
+      if (formData.name === '' || formData.phone === '' || formData.email === '') {
+        return;
+      }});
     
-
+      const Submit = (e) => {
+    // Метод обьекта, для отмены действия браузера по умолчанию, связанного с определённым событием. Отменить переход по ссылке при клике на неё
+      e.preventDefault(); 
+      setNameError(formData.name === '');
+      setPhoneError(formData.phone === '');
+      setEmailError(formData.email === '');
+    }
 
     return (
         <Container
-        style = {{backgroundColor:'#D0D0D0', justifyContent:'space-between', position:'relative', padding:'20px', borderRadius: '15px', width: '1240px', height: '1100px', marginTop:'6px', fontFamily:"Play", paddingTop: '10px'}}>
-        <Card style={{borderRadius: 80, fontFamily:"Play"}} className="p-5 #FFFAF4">
+        style = {{backgroundColor:'#313131', justifyContent:'space-between', position:'relative', padding:'20px', borderRadius: '15px', width: '1240px', height: '1100px', marginTop:'6px', fontFamily:"Play", paddingTop: '10px'}}>
+        <Card style={{borderRadius: 80, fontFamily:"Play", backgroundColor: '#3E3E3E', marginTop: '150px'}} className="p-5 #FFFAF4">
         <Form className="d-flex flex-column" onSubmit={Submit}>
         <Form.Control
-                style={{borderRadius: 70, height: 71, border: "1px solid", fontSize: "24px", marginBottom:'20px'}}
+                style={{borderRadius: 70, backgroundColor: '#7F933A', height: 71, border: "1px solid", fontSize: "24px", marginBottom:'20px'}}
                 className="mt-3"
                 placeholder = "Введите имя..."
                 size="lg"
-                type={formData.name}
-            //     isInvalid={!!errors[key]}
-                onChange={Change}/>
+                onChange={(e) => setFormData({ name: e.target.value })}/>
+                {nameError && <p style={{ color: 'red' }}>Поле не может быть пустым</p>}
           <Form.Control
-                style={{borderRadius: 70, height: 71, border: "1px solid", fontSize: "24px", marginBottom:'20px'}}
+                style={{borderRadius: 70, backgroundColor: '#7F933A', height: 71, border: "1px solid", fontSize: "24px", marginBottom:'20px'}}
                 className="mt-3"
                 placeholder = "Введите номер телефона..."
                 size="lg"
-                type="number"
-                onChange={Change}/>
+                // Это обработчик изменения значения для поля телефона в форме. 
+                // Он позволяет обновлять состояние formData при изменении значения этого поля.
+                onChange={(e) => setFormData({ phone: e.target.value })}/>
+                {phoneError && <p style={{ color: 'red' }}>Поле не может быть пустым</p>}
           <Form.Control
-                style={{borderRadius: 70, height: 71, border: "1px solid", fontSize: "24px", marginBottom:'20px'}}
+                style={{borderRadius: 70, backgroundColor: '#7F933A', height: 71, border: "1px solid", fontSize: "24px", marginBottom:'20px'}}
                 className="mt-3"
                 placeholder = "Введите email адрес..."
                 size="lg"
                 type="email" 
-                onChange={Change}/>
-          <Form.Control
-                style={{borderRadius: 40, height: 71, border: "1px solid", fontSize: "24px", marginBottom:'20px', height: '150px'}}
-                className="mt-3"
-                placeholder = "Ваще сообщение"
-                size="lg"
-                type="text" 
-                onChange={Change}/>
+                onChange={(e) => setFormData({ email: e.target.value })}/>
+                {emailError && <p style={{ color: 'red' }}>Поле не может быть пустым или введен некорректный email</p>}
                 <p style={{marginTop:'30px', display:'flex', justifyContent:'center'}}>
                 <Button
                             type="submit"
